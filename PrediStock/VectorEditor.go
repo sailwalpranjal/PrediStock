a63abc9a57dfa1f88f30f9cc612e70b2d52a4699
Pranjal Sailwal <127615786+sailwalpranjal@users.noreply.github.com>
2024-08-20 02:02:20 +0530

    Update VectorEditor.go

--- PrediStock/VectorEditor.go
+++ PrediStock/VectorEditor.go
@@ -1,7 +1,3 @@
-// Copyright (c) 2013-2024 by Michael Dvorkin and contributors. All Rights Reserved.
-// Use of this source code is governed by a MIT-style license that can
-// be found in the LICENSE file.
-
 package mop
 
 import (
@@ -10,22 +6,15 @@ import (
 
 	"github.com/nsf/termbox-go"
 )
-
-// LineEditor kicks in when user presses '+' or '-' to add or delete stock
-// tickers. The data structure and methods are used to collect the input
-// data and keep track of cursor movements (left, right, beginning of the
-// line, end of the line, and backspace).
 type LineEditor struct {
-	command rune           // Keyboard command such as '+' or '-'.
-	cursor  int            // Current cursor position within the input line.
-	prompt  string         // Prompt string for the command.
-	input   string         // User typed input string.
-	screen  *Screen        // Pointer to Screen.
-	quotes  *Quotes        // Pointer to Quotes.
-	regex   *regexp.Regexp // Regex to split comma-delimited input string.
+	command rune          
+	cursor  int            
+	prompt  string        
+	input   string        
+	screen  *Screen       
+	quotes  *Quotes       
+	regex   *regexp.Regexp 
 }
-
-// Returns new initialized LineEditor struct.
 func NewLineEditor(screen *Screen, quotes *Quotes) *LineEditor {
 	return &LineEditor{
 		screen: screen,
@@ -33,10 +22,6 @@ func NewLineEditor(screen *Screen, quotes *Quotes) *LineEditor {
 		regex:  regexp.MustCompile(`[,\s]+`),
 	}
 }
-
-// Prompt displays a prompt in response to '+' or '-' commands. Unknown commands
-// are simply ignored. The prompt is displayed on the 3rd line (between the market
-// data and the stock quotes).
 func (editor *LineEditor) Prompt(command rune) *LineEditor {
 	filterPrompt := `Set filter: `
 
@@ -59,12 +44,6 @@ func (editor *LineEditor) Prompt(command rune) *LineEditor {
 
 	return editor
 }
-
-// Handle takes over the keyboard events and dispatches them to appropriate
-// line editor handlers. As user types or edits the text cursor movements
-// are tracked in `editor.cursor` while the text itself is stored in
-// `editor.input`. The method returns true when user presses Esc (discard)
-// or Enter (process).
 func (editor *LineEditor) Handle(ev termbox.Event) bool {
 	defer termbox.Flush()
 
@@ -106,13 +85,11 @@ func (editor *LineEditor) Handle(ev termbox.Event) bool {
 func (editor *LineEditor) deletePreviousCharacter() *LineEditor {
 	if editor.cursor > 0 {
 		if editor.cursor < len(editor.input) {
-			// Remove character in the middle of the input string.
 			editor.input = editor.input[0:editor.cursor-1] + editor.input[editor.cursor:len(editor.input)]
 		} else {
-			// Remove last input character.
 			editor.input = editor.input[:len(editor.input)-1]
 		}
-		editor.screen.DrawLine(len(editor.prompt), 3, editor.input+` `) // Erase last character.
+		editor.screen.DrawLine(len(editor.prompt), 3, editor.input+` `) 
 		editor.moveLeft()
 	}
 
@@ -122,10 +99,8 @@ func (editor *LineEditor) deletePreviousCharacter() *LineEditor {
 // -----------------------------------------------------------------------------
 func (editor *LineEditor) insertCharacter(ch rune) *LineEditor {
 	if editor.cursor < len(editor.input) {
-		// Insert the character in the middle of the input string.
 		editor.input = editor.input[0:editor.cursor] + string(ch) + editor.input[editor.cursor:len(editor.input)]
 	} else {
-		// Append the character to the end of the input string.
 		editor.input += string(ch)
 	}
 	editor.screen.DrawLine(len(editor.prompt), 3, editor.input)
@@ -186,8 +161,6 @@ func (editor *LineEditor) execute() *LineEditor {
 			before := len(editor.quotes.profile.Tickers)
 			if removed, _ := editor.quotes.RemoveTickers(tickers); removed > 0 {
 				editor.screen.Draw(editor.quotes)
-
-				// Clear the lines at the bottom of the list, if any.
 				after := before - removed
 				for i := before + 1; i > after; i-- {
 					editor.screen.ClearLine(0, i+4)
@@ -214,9 +187,6 @@ func (editor *LineEditor) done() bool {
 
 	return true
 }
-
-// Split by whitespace/comma to convert a string to array of tickers. Make sure
-// the string is trimmed to avoid empty tickers in the array.
 func (editor *LineEditor) tokenize() []string {
 	input := strings.ToUpper(strings.Trim(editor.input, `, `))
 	return editor.regex.Split(input, -1)
